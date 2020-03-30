622aab4c100d8130cac85e0bebcec9f1ef40ddb2
Michiel de Jong <michiel@unhosted.org>
2020-03-30 15:47:28 +0200

    show tripledoc problem

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export function HelloWorld(): string {
+import { createDocumentInContainer } from "tripledoc";
+
+async function test(): Promise<void> {
+  const doc = createDocumentInContainer(
+    "https://michielbdejong.inrupt.net/inboex/"
+  );
+  const sub = doc.addSubject("#hi");
+  sub.addString("https://example.com/#hello", "world");
+  await doc.save();
+}
+
+export async function HelloWorld(): Promise<string> {
+  await test();
   return "hello, world";
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import { HelloWorld } from "../src/index";
 
 describe("HelloWorld", () => {
-  it("returns hello world", () => {
-    expect(HelloWorld()).toEqual("hello, world");
+  it("returns hello world", async () => {
+    expect(await HelloWorld()).toEqual("hello, world");
   });
 });
